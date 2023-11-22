decf9906b5c3ea72961976a3a4795eeacc36c172
arsaizdihar <arsadihar@gmail.com>
2023-11-22 22:26:59 +0700

    feat: implement parallel

--- server.py
+++ server.py
@@ -2,16 +2,85 @@ import argparse
 import math
 import os
 import socket
+import typing
 from connection import get_seqnum_diff
 from node import MessageInfo, Node
 from segment import MAX_PAYLOAD, Segment, SegmentError
+import threading
+
+ENABLE_PARALLEL = True
+
+class PausableBroadcastThread(threading.Thread):
+  def __init__(self, server, addr: tuple[str, int]):
+    super().__init__()
+    self.server = server
+    self.addr = addr
+    self.is_paused = False
+    self.is_stopped = False
+    self.pause_cond = threading.Condition(threading.Lock())
+
+  def run(self):
+    with self.pause_cond:
+      while self.is_paused:
+        self.pause_cond.wait()
+      file = open(server.file_path, "rb")
+      max_segment = math.ceil(server.filesize / MAX_PAYLOAD)
+      addr = self.addr
+      conn = server.handshake(addr[0], addr[1])
+      sent_segment = 0
+    while sent_segment < max_segment:
+      with self.pause_cond:
+        while self.is_paused:
+          self.pause_cond.wait()
+        to_send = min(conn.send.window_size, max_segment - sent_segment)
+        for i in range(to_send):
+          file.seek((sent_segment + i) * MAX_PAYLOAD)
+          print(f"[Segment SEQ={conn.send.seq_num + i}] Sent")
+          server.send(conn.send.remote_ip, conn.send.remote_port, Segment.payload(conn.send.seq_num + i, file.read(MAX_PAYLOAD)))
+
+        start_seq_num = conn.send.seq_num
+        start_sent_segment = sent_segment
+        i = 0
+      while i < to_send:
+        with self.pause_cond:
+          while self.is_paused:
+            self.pause_cond.wait()
+          for i in range(to_send):
+            try:
+              addr, segment = server.listen(5)
+              if segment is not None and segment.flags.ack and addr == (conn.send.remote_ip, conn.send.remote_port):
+                print(f"[Segment SEQ={segment.ack_num-1}] Ack received", end="")
+                diff = get_seqnum_diff(start_seq_num, conn.send.seq_num)
+                if start_sent_segment + diff > sent_segment:
+                  print(f", new sequence base = {segment.ack_num}")
+                  sent_segment = start_sent_segment + diff
+                else:
+                  print()
+                i += 1
+                if diff == to_send:
+                  break
+            except socket.timeout:
+              i = to_send
+              break
+      print(f"[!] Finished sending to {addr[0]}:{addr[1]}")
+      server.end_connection(conn.send.remote_ip, conn.send.remote_port)
+
+  def pause(self):
+    if not self.is_paused:
+      self.is_paused = True
+      self.pause_cond.acquire()
+  
+  def resume(self):
+    self.is_paused = False
+    self.pause_cond.notify()
+    self.pause_cond.release()
 
 
 class Server(Node):
   def __init__(self, ip: str, port: int, file_path: str) -> None:
     super().__init__(ip, port)
     self.register_handler(self.handle_message)
-    self.listen_addresses: list[tuple[str, int]] = []
+    self.listen_addresses: dict[tuple[str, int], typing.Optional[PausableBroadcastThread]] = {}
     self.file_path = file_path
     self.filesize = os.path.getsize(file_path)
   
@@ -19,20 +88,35 @@ class Server(Node):
     listening = True
     print("[!] Listening for broadcast request for clients.\n")
     while listening:
+        addr = None
         try:
-          self.listen_broadcast()
-        except socket.timeout:
+          if ENABLE_PARALLEL:
+            for addr, thread in self.listen_addresses.items():
+              if thread is not None:
+                thread.resume()
+          addr = self.listen_broadcast()
+          if addr is not None and ENABLE_PARALLEL:
+            self.listen_addresses[addr] = PausableBroadcastThread(self, addr)
+            self.listen_addresses[addr].pause()
+            self.listen_addresses[addr].start()
+        except socket.error:
           print("Timeout")
-        is_listen_more = input("[?] Listen more? (y/n) ")
-        if is_listen_more == "n":
+        for addr, thread in self.listen_addresses.items():
+          if thread is not None:
+            thread.pause()
+        is_listen_more = input("[?] Listen more? (y/N) ")
+        if is_listen_more != "y":
             break
 
   def listen_broadcast(self):
     try:
-      addr, segment, valid_checksum = self.listen_base(30)
+      addr, segment, valid_checksum = self.listen_base(5)
       if valid_checksum and segment.flags.syn and not segment.flags.ack and addr not in self.listen_addresses:
-        self.listen_addresses.append(addr)
+        self.listen_addresses[addr] = None
         print(f"[!] Received request from {addr[0]}:{addr[1]}")
+        return addr
+      else:
+        return self.listen_broadcast()
     except SegmentError as e:
       print(e)
 
@@ -83,6 +167,7 @@ class Server(Node):
     print(message.segment)
     print("==========================")
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("port", type=int)
@@ -92,4 +177,13 @@ if __name__ == '__main__':
     server = Server('127.0.0.1', args.port, args.input_path)
     
     server.run()
-    server.broadcast()
+    if ENABLE_PARALLEL:
+      for addr, thread in server.listen_addresses.items():
+        if thread is not None:
+          thread.resume()
+      for addr, thread in server.listen_addresses.items():
+        if thread is not None:
+          thread.join()
+    else:
+      server.broadcast()
+    print("[!] Finished broadcasting")
