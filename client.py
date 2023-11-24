6882b24a1c9d06e042cc96d37e385db40a24257d
Kenneth Ezekiel <88850771+KenEzekiel@users.noreply.github.com>
2023-11-24 16:22:54 +0700

    add: bonus metadata

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 from node import Node, MessageInfo
 from segment import Segment, SegmentFlags
 
@@ -10,6 +11,7 @@ class Client(Node):
     self.server_ip = server_ip
     self.server_port = server_port
     self.data: list[bytes] = []
+    self.is_first = True
   
   # def send(self, segment: Segment):
   #   self.send(self.server_ip, self.server_port, segment)
@@ -30,20 +32,30 @@ class Client(Node):
     return self.connections.get((self.server_ip, self.server_port))
 
   def handle_message(self, message_info: MessageInfo):
-    self.data.append(message_info.segment.payload)
+    if self.is_first:
+      metadata_segment, checksum = Segment.from_bytes(message_info.segment.payload)
+      print("checksum", metadata_segment.is_valid_checksum())
+      print(str(metadata_segment))
+      print(metadata_segment.payload.decode())
+      metadata = json.loads(metadata_segment.payload.decode())
+      self.output_path = metadata['filename'] + 'a.' + metadata['extension']
+      self.is_first = False
+      print(self.output_path, "is now detected")
+    else:
+      self.data.append(message_info.segment.payload)
     print(f"[Segment SEQ={message_info.segment.seq_num}] Received, Ack sent")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("client_port", type=int)
     parser.add_argument("server_port", type=int)
-    parser.add_argument("output_path", type=str)
+    # parser.add_argument("output_path", type=str)
     args = parser.parse_args()
 
     client = Client('127.0.0.1', args.client_port, "127.0.0.1", args.server_port)
-    output_path = args.output_path
+    # output_path = args.output_path
     client.run()
 
-    with open(output_path, "wb") as f:
+    with open(client.output_path, "wb") as f:
       for data in client.data:
         f.write(data)
